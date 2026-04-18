154febe8a96e94bb416a2574b72fd1926c6d0f75
Mohammad Ayas <mohammadayas@MacBook-Air-M4.local>
2026-04-18 10:57:47 +0600

    Create route file

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
+import router from "./app/router/router";
 
 const app = express();
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/v1", router);
 
 // test route
 app.get("/", (req, res) => {
