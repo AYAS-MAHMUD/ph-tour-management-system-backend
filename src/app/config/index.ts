767813cd9d8327d54d24787dd8c73bbb26d30494
Mohammad Ayas <mohammadayas@MacBook-Air-M4.local>
2026-04-29 16:30:03 +0600

    Added seed Super Admin

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -9,5 +9,7 @@ export const config = {
   db_url: process.env.DB_URL,
   node_env : process.env.NODE_ENV,
   jwt_secret : process.env.JWT_SECRET,
-  jwt_expires_in : process.env.JWT_EXPIRES_IN
+  jwt_expires_in : process.env.JWT_EXPIRES_IN,
+  super_admin_email : process.env.SUPER_ADMIN_EMAIL,
+  super_admin_password : process.env.SUPER_ADMIN_PASSWORD
 };
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 import http from 'http';
 import app from './app';
 import { config } from './app/config';
+import { seedSuperAdmin } from './utils/seedSuperAdmin';
 
 // const b ;
 
@@ -11,8 +12,9 @@ async function main() {
   try {
     await mongoose.connect(config.db_url as string);
     console.log('✅ MongoDB Connected');
-
+    await seedSuperAdmin();
     server.listen(config.port, () => {
+      
       console.log(`🚀 Server running on port ${config.port}`);
     });
   } catch (error) {
